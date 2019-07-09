181266141c996af404ebd6408faceab09bb631d9
Paul Taylor <ptaylor@ricoh.com.au>
2019-07-10 08:02:40 +1000

    add template formatting

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"strconv"
 )
@@ -13,7 +12,16 @@ func (app *App) Home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	app.RenderHTML(w, "home.page.html") // Use the app.RenderHTML() helper.
+	// Fetch a slice of the latest snippets from the database.
+	snippets, err := app.Database.LatestSnippets()
+	if err != nil {
+		app.ServerError(w, err)
+		return
+	}
+	// Pass the slice of snippets to the "home.page.html" templates.
+	app.RenderHTML(w, r, "home.page.html", &HTMLData{
+		Snippets: snippets,
+	})
 
 }
 
@@ -34,7 +42,9 @@ func (app *App) ShowSnippet(w http.ResponseWriter, r *http.Request) {
 		app.NotFound(w)
 		return
 	}
-	fmt.Fprint(w, snippet)
+	// Render the show.page.html template, passing in the snippet data wrapped in our HTMLData struct.
+	app.RenderHTML(w, r, "show.page.html", &HTMLData{
+		Snippet: snippet})
 }
 
 // Change the signature of our NewSnippet handler so it is defined as a method against *App.
--- cmd/web/views.go
+++ cmd/web/views.go
@@ -1,26 +1,70 @@
 package main
 
 import (
+	"bytes"
 	"html/template"
 	"net/http"
 	"path/filepath"
+	"sinistra/snippetbox/models"
+	"time"
 )
 
-func (app *App) RenderHTML(w http.ResponseWriter, page string) {
+// Define a new HTMLData struct to act as a wrapper for the dynamic data we want
+// to pass to our templates. For now this just contains the snippet data that we
+// want to display, which has the underling type *models.Snippet.
+type HTMLData struct {
+	Path     string
+	Snippet  *models.Snippet
+	Snippets []*models.Snippet
+}
+
+// Create a humanDate function which returns a nicely formatted string
+// representation of a time.Time object.
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 15:04")
+}
+
+// Update the signature of RenderHTML() so that it accepts a new data parameter
+// containing a pointer to a HTMLData struct.
+func (app *App) RenderHTML(w http.ResponseWriter, r *http.Request, page string, data *HTMLData) {
+	// If no data has been passed in, initialize a new empty HTMLData object.
+	if data == nil {
+		data = &HTMLData{}
+	}
+	// Add the current request URL path to the data.
+	data.Path = r.URL.Path
+
 	files := []string{
 		filepath.Join(app.HTMLDir, "base.html"),
-		filepath.Join(app.HTMLDir, page),
-	}
+		filepath.Join(app.HTMLDir, page)}
 
-	ts, err := template.ParseFiles(files...)
+	// Initialize a template.FuncMap object. This is essentially a string-keyed map
+	// which acts as a lookup between the names of our custom template functions and
+	// the functions themselves.
+	fm := template.FuncMap{
+		"humanDate": humanDate}
+
+	// Our template.FuncMap must be registered with the template set before we call
+	// the ParseFiles() method. This means we have to use template.New() to create
+	// an empty, unnamed, template set, use the Funcs() method to register our
+	// template.FuncMap, and then parse the files as normal.
+	ts, err := template.New("").Funcs(fm).ParseFiles(files...)
 	if err != nil {
-		app.ServerError(w, err) // Use the new app.ServerError() helper.
+		app.ServerError(w, err)
 		return
 	}
 
-	err = ts.ExecuteTemplate(w, "base", nil)
+	// Initialize a new buffer.
+	buf := new(bytes.Buffer)
+	// Write the template to the buffer, instead of straight to the
+	// http.ResponseWriter. If there's an error, call our error handler and then return.
+	err = ts.ExecuteTemplate(buf, "base", data)
 	if err != nil {
-		app.ServerError(w, err) // Use the new app.ServerError() helper.
+		app.ServerError(w, err)
+		return
 	}
-
+	// Write the contents of the buffer to the http.ResponseWriter.
+	// Again, this is another time where we pass our http.ResponseWriter to a function that
+	// takes an io.Writer.
+	buf.WriteTo(w)
 }
