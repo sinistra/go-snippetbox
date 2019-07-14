4a10987ad72b4d8fd2e60f5a85291959c1f4337b
Paul Taylor <ptaylor@ricoh.com.au>
2019-07-15 07:19:07 +1000

    add middleware

---
+++ cmd/web/middleware.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func LogRequest(next http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		pattern := `%s - "%s %s %s"`
+		log.Printf(pattern, r.RemoteAddr, r.Proto, r.Method, r.URL.RequestURI())
+		next.ServeHTTP(w, r)
+	}
+	return http.HandlerFunc(fn)
+}
+
+func SecureHeaders(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("X-Content-Type-Options", "nosniff")
+		w.Header().Set("X-Frame-Options", "deny")
+		w.Header()["X-XSS-Protection"] = []string{"1; mode=block"}
+		next.ServeHTTP(w, r)
+	})
+}
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -18,7 +18,9 @@ func (app *App) Routes() http.Handler {
 	fileServer := http.FileServer(http.Dir(app.StaticDir))
 	mux.Get("/static/", http.StripPrefix("/static", fileServer))
 
-	// Return the serve mux.
-	return mux
+	// Pass the router as the 'next' parameter to the LogRequest middleware.
+	// Because LogRequest() is just a function, and the function returns a
+	// http.Handler we don't need to do anything else.
+	return LogRequest(SecureHeaders(mux))
 
 }
